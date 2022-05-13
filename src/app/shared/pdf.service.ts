96d0401e9bdf8437808e180c05cbd79cd8dd38bc
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2022-05-13 16:09:33 +0500

    Update pdf.service.ts

--- src/app/shared/pdf.service.ts
+++ src/app/shared/pdf.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { Pdf } from './pdf';
+// import { Observable } from 'rxjs';
+// import { Pdf } from './pdf.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PdfService {
+  apiUrl: 'http://localhost:3000/posts';
   constructor(public _http: HttpClient) {}
-  getAlldata(): Observable<Pdf[]> {
-    return this._http.get<Pdf[]>('http://localhost:3000/' + 'Product');
+
+  getAlldata() {
+    return this._http.get(this.apiUrl);
   }
 }
