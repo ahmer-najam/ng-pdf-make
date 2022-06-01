726d753751fe31ee9e68d0a9186265827dacd313
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2022-06-01 14:25:40 +0500

    Update pdf.service.ts

--- src/app/shared/pdf.service.ts
+++ src/app/shared/pdf.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Pdf } from './pdf.model';
+import { Product } from './product.model';
 // import { Observable } from 'rxjs';
 // import { Pdf } from './pdf.model';
 
@@ -10,7 +13,7 @@ export class PdfService {
   apiUrl: 'http://localhost:3000/posts';
   constructor(public _http: HttpClient) {}
 
-  getAlldata() {
-    return this._http.get(this.apiUrl);
+  getAlldata(): Observable<Product[]> {
+    return this._http.get<[Product]>(this.apiUrl);
   }
 }
